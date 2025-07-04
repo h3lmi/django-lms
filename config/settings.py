167232c7db4649ae085b0aee984e27494badc425
helmi <32284907+h3lmi@users.noreply.github.com>
2025-07-05 02:46:34 +0800

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -59,7 +59,7 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = "ebm_lms.urls"
+ROOT_URLCONF = "config.urls"
 
 TEMPLATES = [
     {
@@ -77,7 +77,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = "ebm_lms.wsgi.application"
+WSGI_APPLICATION = "config.wsgi.application"
 
 
 # Database
