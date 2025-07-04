869fd73ca10bb5b7449a19fe62c0f51742e5b94c
helmi <32284907+h3lmi@users.noreply.github.com>
2025-07-05 00:53:52 +0800

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,8 @@
+from decouple import config
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = config('EMAIL_HOST', default='smtp.gmail.com')
+EMAIL_PORT = config('EMAIL_PORT', default=587, cast=int)
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=True, cast=bool)
+EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='placeholder@example.com')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='placeholder')
