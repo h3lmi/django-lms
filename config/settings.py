89e5d0f98bc44d1e6eae6bd4bbe4367015e86464
helmi <32284907+h3lmi@users.noreply.github.com>
2025-07-05 01:01:27 +0800

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -196,7 +196,7 @@ EMAIL_BACKEND = config(
 EMAIL_HOST = config("EMAIL_HOST", default="smtp.gmail.com")
 EMAIL_PORT = config("EMAIL_PORT", default=587)
 EMAIL_USE_TLS = config("EMAIL_USE_TLS", default=True, cast=bool)
-EMAIL_HOST_USER = config("EMAIL_HOST_USER")
+EMAIL_HOST_USER = config("EMAIL_HOST_USER", default="sigmainfoanalytix@gmail.com")
 EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
 EMAIL_FROM_ADDRESS = config("EMAIL_FROM_ADDRESS")
 EMAIL_USE_SSL = False
